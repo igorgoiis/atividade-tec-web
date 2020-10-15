e7c9cfec5802116b0cf6db6016820edd4d957ef6
igorgoiis <ig_orgabriel@hotmail.com>
2020-10-15 01:56:04 -0300

    JavaScript em andamento

--- script.js
+++ script.js
@@ -1,10 +1,63 @@
-function newPopup() {
-  varWindow = window.open (
-    'popup.html',
-    'popup',
-    "width=900, height=500, top=120, left=250, scrollbars=no");
+let db = [];
+
+agora = new Date;
+
+data = agora.getDate() + '/' + agora.getMonth() + '/' + agora.getFullYear();
+console.log(data);
+
+function cadastrar() {
+  data = agora.getDate() + '/' + agora.getMonth() + '/' + agora.getFullYear();
+  titulo = document.getElementById('titulo').value;
+  tipo = document.getElementById('tipo').value;
+  valor = document.getElementById('valor').value;
+  categoria = document.getElementById('categoria').value;
+  
+  
+  dados = {
+    titulo,
+    tipo,
+    valor,
+    categoria,
+    data
+  }
+  
+  db.push(dados);
+  mostrar();
+  entradas();
 }
 
-function closePopup() {
-  fecharWindow = varWindow.close();
+function mostrar() {
+  inserir = '';
+  for (let i = 0; i < db.length; i++) {
+    const element = db[i];
+    tabela = document.getElementById('tabela');
+    inserir = inserir + '<tr><td><span>' + element.titulo + 
+    '</span></td> <td><span>R$' + element.valor +
+    '</span></td><td><span>' + element.categoria + 
+    '</span></td><td><span>' + element.data + '</span></td></tr>';
+    
+  }
+  tabela.innerHTML = inserir;
+}
+
+function entradas() {
+  let totalEntrada = 0;
+  let totalSaida = 0;
+  for (let i = 0; i < db.length; i++) {
+    const element = db[i];
+    console.log(element.tipo)
+    if (element.tipo == 'entrada') {
+      totalEntrada = totalEntrada + parseFloat(element.valor);
+    } else {
+      totalSaida = totalSaida + parseFloat(element.valor);
+    }
+  }
+  valorTotal = totalEntrada - totalSaida;
+  entrada = document.getElementById('total-entrada');
+  saida = document.getElementById('total-saida');
+  total = document.getElementById('total-total');
+
+  entrada.innerHTML = 'R$ ' + totalEntrada;
+  saida.innerHTML = 'R$ ' + totalSaida;
+  total.innerHTML = 'R$ ' + valorTotal;
 }
